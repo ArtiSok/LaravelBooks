1f53ee601b7cb8d5c1eb879ffdd344f478a4d45a
Artur Sokolov <t154637@vk.edu.ee>
2018-10-23 11:44:41 +0300

    new

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('books')->insert([
+            'title' => 'Java',
+            'description' => 'descJava',
+            'category_id' => 1
+        ]);
+        DB::table('books')->insert([
+            'title' => 'C++',
+            'description' => 'descC++',
+            'category_id' => 1
+        ]);
+        DB::table('books')->insert([
+            'title' => 'C#',
+            'description' => 'descC#',
+            'category_id' => 1
+        ]);
+        DB::table('books')->insert([
+            'title' => 'Beginning Angular with Typescript',
+            'description' => 'desc Beginning Angular with Typescript',
+            'category_id' => 2
+        ]);
+        DB::table('books')->insert([
+            'title' => 'PHP and MySQL Web Devolopment',
+            'description' => 'desc PHP and MySQL Web Devolopment',
+            'category_id' => 2
+        ]);
+        DB::table('books')->insert([
+            'title' => 'MySQL Tutorial',
+            'description' => 'desc MySQL Tutorial',
+            'category_id' => 3
+        ]);
+
+    }
+}
